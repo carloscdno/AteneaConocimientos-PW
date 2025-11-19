f28df5c645d7e065c31baf196a4e08c624b461e6
Juan Torres <juantor16@gmail.com>
2025-11-19 15:23:32 -0300

    Se agrega escenario logout de estudiante y administrador

--- pages/paginaDashboard.ts
+++ pages/paginaDashboard.ts
@@ -3,10 +3,14 @@ import { Locator, Page } from '@playwright/test';
 export class PaginaDashboard {
     readonly page: Page;
     readonly linkMisTalleres: Locator;
+    readonly botonMenuUsuario: Locator;
+    readonly opcionCerrarSesion: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.linkMisTalleres = page.getByRole('tab', { name: 'Mis Talleres' });
+        this.botonMenuUsuario = page.getByRole('button', { name: /account of current user/i });
+        this.opcionCerrarSesion = page.getByRole('menuitem', { name: 'Cerrar Sesión' });
     }
 
     async navegarADashboard() {
@@ -15,5 +19,15 @@ export class PaginaDashboard {
 
     async esperarDashboardVisible() {
         await this.linkMisTalleres.waitFor({ state: 'visible' });
+    }
+
+    async abrirMenuUsuario() {
+        await this.botonMenuUsuario.click();
+        await this.opcionCerrarSesion.waitFor({ state: 'visible' });
+    }
+
+    async cerrarSesionDesdeMenu() {
+        await this.abrirMenuUsuario();
+        await this.opcionCerrarSesion.click();
     }
 }
